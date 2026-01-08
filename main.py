5b703e7a5e8a81b957915762ca441af87eb74d2d
Sonia <01200993@pw.edu.pl>
2026-01-08 22:06:45 +0100

    Decided to get rid of the file simulation.py and from class Similator because it was sensless. Rebuilt the whole structure of the file main.py for it to work without this class

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from ant import Ant
-from simulator import Simulator
 from PIL import Image
 import os
 import random
@@ -11,24 +10,18 @@ class Configuration:
     def __init__(self):
         '''
         Konstruktor klasy Configuration
-        Zawiera niezbędnę dla symulacji atrybuty 
+        Zawiera niezbędnę dla symulacji atrybuty
         '''
-        self.simulator = Simulator()
-        self.height = self.simulator.window_height
-        self.width = self.simulator.window_width
-        if self.simulator.image_sel:
-            self.background = self.simulator.users_image
-        else:
-            size = (self.width, self.height)
-            self.background = Image.new('RGB', size, 'white')
-        self.pixels_access = self.background.load()
-        self.ant = Ant(self.background, self.width, self.height)
-        self.steps = self.simulator.steps
-        self.chance_black = self.simulator.chance_black
+        self.window_height = 0
+        self.window_width = 0
+        self.chance_black = 0
+        self.steps = 0
+        self.image_sel = False
+        self.users_image = None
         self.result_folder = "result"
-        if os.path.exists(self.result_folder):
-            os.system(f'rm -rf "{self.result_folder}"')
-        os.makedirs(self.result_folder)
+        self.background = None
+        self.ant = None
+        self.pixels_access = None
 
     def random_black_change(self):
         '''
@@ -37,15 +30,87 @@ class Configuration:
         prawdopodobieństwa wystąpienia czarnego
         '''
         if self.chance_black > 0:
-            for x in range(self.width):
-                for y in range(self.height):
+            for x in range(self.window_width):
+                for y in range(self.window_height):
                     if random.randint(0, 100) < self.chance_black:
                         self.pixels_access[x, y] = (0, 0, 0)
+
+    def console_information(self):
+        '''
+        Metoda dla wypisywania komunikatów,pobrania ścieżki obrazu,
+        liczby kroków,wymiarów planszy,prawdopodobieństwa czarnych pól
+        '''
+        print("---KONFIGURACJA MRÓWKI LANGTONA---")
+        print("Wybierz tryb: ")
+        print("1 - Białe tło")
+        print("2 - Własny czarno-biały obraz")
+        print("3 - Losowo wygenerowany czarno-biały obraz")
+        mode = int(input("Twój wybór (1-3): "))
+        if self.check_int(mode, 'mode'):
+            if mode == 2:
+                path = input("Podaj ścieżkę do obrazka: ")
+                if os.path.exists(path):
+                    self.image_sel = True
+                    self.users_image, self.window_width, self.window_height \
+                        = self.load_and_process_image(path)
+            else:
+                self.window_width = int(input("Podaj szerokość planszy: "))
+                self.window_height = int(input("Podaj wysokość planszy: "))
+                if self.check_int(self.window_width) \
+                        and self.check_int(self.window_height):
+                    if mode == 3:
+                        chance = int(input("Podaj czarnych pól: "))
+                        if self.check_int(chance, 'chance'):
+                            self.chance_black = chance
+                else:
+                    raise ValueError('Podana wartość nie jest poprawna')
+        else:
+            raise ValueError('Podana wartość nie jest poprawna')
+        steps = int(input("Podaj liczbę kroków symulacji: "))
+        if self.check_int(steps):
+            self.steps = steps
+        else:
+            raise ValueError('Podana wartość nie jest poprawna')
+
+    def check_int(self, input, which_value='standard_int'):
+        '''
+        Sprawdza liczbę, podaną przez użytkownika,zgodnie z wymaganiami
+        '''
+        if which_value == 'mode':
+            return input in [1, 2, 3]
+        elif which_value == 'chance':
+            return 0 <= input <= 100
+        return input > 1
+
+    def load_and_process_image(self, path):
+        """
+        Wczytuje obraz, przetwarza go na format czarno-biały
+        i zwraca obiekt obrazu oraz jego wymiary.
+        """
+        try:
+            img = Image.open(path).convert('RGB')
+            img = img.convert('1').convert('RGB')
+            width, height = img.size
+            return img, width, height
+        except FileNotFoundError:
+            print("Nie znaleziono pliku")
+        except Exception:
+            print("Plik jest uszkodzony")
 
     def start(self):
         '''
         Główna pętla symulacji
         '''
+        if os.path.exists(self.result_folder):
+            os.system(f'rm -rf "{self.result_folder}"')
+        os.makedirs(self.result_folder)
+        if self.image_sel and self.users_image:
+            self.background = self.users_image
+        else:
+            size = (self.window_width, self.window_height)
+            self.background = Image.new('RGB', size, 'white')
+        self.pixels_access = self.background.load()
+        self.ant = Ant(self.background, self.window_width, self.window_height)
         self.random_black_change()
         # zapisuje pocztkową pozycję mrówki na planszy jako krok_0.png
         save = self.background.copy()
@@ -68,5 +133,6 @@ class Configuration:
 
 
 if __name__ == '__main__':
-    configuration = Configuration()
-    configuration.start()
+    sim = Configuration()
+    sim.console_information()
+    sim.start()
